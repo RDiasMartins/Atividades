35892dee8f0bf058e109c6ebd31ae76a98d8379d
RDiasMartins <renan2204@outlook.com>
2020-02-19 20:57:31 -0300

    Repositório para atividades WEB - IFSP_ARQ

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
-<h2> Links das minhas atividades </h2>
-<hr>
-<br/>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title> Minhas Atividades - Renan </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="estilo.min.css" />
+    <link href = "https://fonts.googleapis.com/icon?family=Material+Icons" rel = "stylesheet"/>
+</head>
+<body>
+    <div class="container-fluid">
+        <?php
+        include "menu.inc";
+        ?>
+        <br/>
+
+        <table class="table table-light table-striped table-hover">
+            <thead class="thead-dark">
+                <tr>
+                    <th>#</th>
+                    <th>Data</th>
+                    <th>Atividade</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>?</td>
+                    <td>??-??-????</td>
+                    <td> ? </td>
+                </tr>
+            </tbody>
+</body>
+</html>
